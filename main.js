3582da11d2353556a2d002bdcc3df57f95c4291b
Across_Coding <105272087+ChaanBiz@users.noreply.github.com>
2023-03-16 09:24:07 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const menuToggle = document.querySelector('.toggle');
+    const navBar = document.querySelector('.nav');
+    menuToggle.classList.toggle('active');
+    navBar.classList.toggle('active');
+}
